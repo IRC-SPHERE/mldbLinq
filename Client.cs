b348d1a7a7076c3c33be8cb5a768634dd6354640
Tom Diethe <tdiethe@gmail.com>
2017-06-23 12:00:38 +0100

    First commit - basic project structure

---
+++ Client.cs
@@ -0,0 +1,24 @@
+﻿namespace mldbLinq
+{
+    using Shaolinq;
+    using Shaolinq.Postgres;
+    
+    public class Client
+    {
+        public string ConnectionString { get; set; } =
+            "Server=127.0.0.1;Port=5432;Database=myDataBase;User Id=myUsername;Password=myPassword;";
+
+        public void Connect()
+        {
+            var configuration = PostgresConfiguration.Create(ConnectionString);
+            var model = DataAccessModel.BuildDataAccessModel<MldbModel>(configuration);
+        }
+    }
+
+    [DataAccessModel]
+    public abstract class MldbModel : DataAccessModel
+    {
+        [DataAccessObjects]
+        public abstract DataAccessObjects<>
+    }
+}
